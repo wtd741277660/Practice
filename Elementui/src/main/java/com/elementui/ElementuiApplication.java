9456afbe05718cf6e4b941fd866c4e3ccb50820a
wtd741277660 <wtd91081511>
2019-05-12 15:34:04 +0800

    添加elementui模块

---
+++ Elementui/src/main/java/com/elementui/ElementuiApplication.java
@@ -0,0 +1,13 @@
+package com.elementui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementuiApplication.class, args);
+	}
+
+}
