461fedd578c5b3f18f743bfee44a67afd862c743
wtd741277660 <wtd91081511>
2019-05-12 20:04:32 +0800

    添加elementui模块

---
+++ Elementui/web/element-starter-master/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <el-button @click="startHacking">Start</el-button>
+    </div>
+    <div class="block">
+      <span class="demonstration">显示默认颜色</span>
+      <span class="wrapper">
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    </span>
+    </div>
+    <br/>
+    <div class="block">
+      <span class="demonstration">hover 显示颜色</span>
+      <span class="wrapper">
+    <el-button :plain="true" type="success">成功按钮</el-button>
+    <el-button :plain="true" type="warning">警告按钮</el-button>
+    <el-button :plain="true" type="danger">危险按钮</el-button>
+    <el-button :plain="true" type="info">信息按钮</el-button>
+    </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
