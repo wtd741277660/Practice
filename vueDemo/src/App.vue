d21ab1762335818828375cab361c9b9679907464
wtd741277660 <wtd91081511>
2019-05-22 16:32:10 +0800

    添加demo组件

--- vueDemo/src/App.vue
+++ vueDemo/src/App.vue
@@ -4,16 +4,29 @@
 <!--    <router-view/>-->
 <!--    <router-view>/myComponent</router-view>-->
 <!--    <KindEditor></KindEditor>-->
-    <my-component></my-component>
+<!--    <my-component></my-component>-->
+    <!--数字输入框组件-->
+    <input-number v-model="value" :min="0" :max="100" :step="10"></input-number>
   </div>
 </template>
 
 <script>
 import KindEditor from "./components/KindEditor";
 import MyComponent from "./components/myComponent";
+import InputNumber from "./components/demo/InputNumber";
 export default {
   name: 'App',
-  components: {MyComponent, KindEditor},
+  components: {InputNumber, MyComponent, KindEditor},
+  data(){
+    return {
+      value:5
+    }
+  },
+  watch:{
+    value(val,old){
+      console.info(old + ">>>" + val)
+    }
+  }
 }
 </script>
 
---
+++ vueDemo/src/components/demo/InputNumber.vue
@@ -0,0 +1,98 @@
+<template>
+    <div id="input-number">
+      <input :value="currentValue"
+             @change="handleChange"
+             @keyup.up="handlePlus"
+             @keyup.down="handleDown"
+      />
+      <button @click="handlePlus"
+              :disabled="currentValue >= max"
+      >
+        +</button>
+      <button @click="handleDown"
+              :disabled="currentValue <= min"
+      >
+        -</button>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "InputNumber",
+      props:{
+        min:{
+          type:Number,
+          default:Infinity
+        },
+        max:{
+          type:Number,
+          default:Infinity
+        },
+        value:{
+          type:Number,
+          default:0
+        },
+        step:{
+          type:Number,
+          default:1
+        }
+      },
+      data(){
+        return {
+          currentValue:this.value,
+        }
+      },
+      watch:{
+        currentValue:function(val){
+          this.$emit("input",val);
+        },
+        value:function(val){
+          this.updateValue(val);
+        }
+      },
+      methods:{
+        handlePlus(){
+          if(this.currentValue >= this.max){
+            return;
+          }
+          this.currentValue+=this.step;
+        },
+        handleDown(){
+          if(this.currentValue <= this.min){
+            return;
+          }
+          this.currentValue-=this.step;
+        },
+        handleChange(event){
+          var val = event.target.value.trim();
+          var max = this.max;
+          var min = this.min;
+          val = Number(val);
+
+          if(val > max){
+            this.currentValue = max;
+          }else if(val < min){
+            this.currentValue = min;
+          }else{
+            this.currentValue = val;
+          }
+        },
+        updateValue(val){
+          if(val >= this.max){
+            val = this.max;
+          }
+          if(val <= this.min){
+            val = this.min;
+          }
+          this.currentValue = val;
+        }
+      },
+      mounted() {
+        this.updateValue(this.value);
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
